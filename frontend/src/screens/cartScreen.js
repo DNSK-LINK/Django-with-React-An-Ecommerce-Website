5bfbfe04168a351633f7d0439a3690ca9d724132
DNSK-LINK <122219376+DNSK-LINK@users.noreply.github.com>
2023-05-27 13:14:31 +0300

    Add to Cart - TotalItems and TotalPrice

--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -25,8 +25,9 @@ function CartScreen() {
         }
     }, [dispatch, productId, qty])
 
-
-
+    const removeFromCartHandler = (id) => {
+        console.log('Removing:', id)
+    }
 
     return (
         <Row>
@@ -42,7 +43,7 @@ function CartScreen() {
                             <ListGroup.Item key={item.product}>
                                 <Row>
                                     <Col md={2}>
-                                        <Image src={item.image} />
+                                        <Image src={item.image} alt={item.name} fluid rounded/>
                                     </Col>
                                     
                                     <Col md={3}>
@@ -52,6 +53,33 @@ function CartScreen() {
                                     <Col md={2}>
                                         ${item.price}
                                     </Col>
+
+                                    <Col md={2}>
+                                        <Form.Control 
+                                            as="select"
+                                            value={item.qty}
+                                            onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                                        >
+                                            {
+                                                [...Array(item.countInStock).keys()].map((x) => (
+                                                    <option key={ x + 1 } value={ x + 1 }>
+                                                        { x + 1 }
+                                                    </option>
+                                                ))
+                                            }
+                                        </Form.Control>
+                                    </Col>
+
+                                    <Col md={1}>
+                                        <Button
+                                            type='button'
+                                            variant='light'
+                                            onClick={() => removeFromCartHandler(item.product)}
+                                        >
+                                            <i className='fas fa-trash'></i>
+                                        </Button>
+                                    </Col>
+
                                 </Row>
                             </ListGroup.Item>
                         ))}
@@ -60,7 +88,19 @@ function CartScreen() {
             </Col>
 
             <Col md={4}>
-                
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2 className='text-center'>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+                            <h3 className='text-center'>
+                                For&nbsp;&nbsp;
+                                <span className='price-card-span'>
+                                    $&nbsp;{cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2).toString()}
+                                </span>
+                            </h3>                           
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
             </Col>
         </Row>
     ) 
